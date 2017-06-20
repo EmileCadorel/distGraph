cb69d43eae1ebca78812af02b2bf8ec69c6174c5
Emile Cadorel <ecadorel@gmail.com>
2017-06-20 16:03:18 +0200

    mise a jour configure

--- configure.sh
+++ configure.sh
@@ -1,11 +1,11 @@
 
-if [ ! -d  "${HOME}/libs" ]; then
-    mkdir ${HOME}/libs
-    echo "Creation du dossier ~/libs"
-fi
+# if [ ! -d  "${HOME}/libs" ]; then
+#     mkdir ${HOME}/libs
+#     echo "Creation du dossier ~/libs"
+# fi
 
-cd libs
-./install.sh
-cd ..
+#cd libs
+#./install.sh
+#cd ..
 dub add-local .
-cp findPort.sh ~/libs/
+# cp findPort.sh ~/libs/
